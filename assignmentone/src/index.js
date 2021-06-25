e5d024fe541f859d1acdfafc12261c46a76b4255
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-25 15:22:04 +0000

    having the swap right message

--- assignmentone/src/index.js
+++ assignmentone/src/index.js
@@ -27,33 +27,40 @@ function App() {
     var zeroindex = 0;
     var zerosindex = 0;
 
-    for (let i = 0; i < board.length; i++) {
-      for (let t = 0; t < board.length; t++) {
-        if (board[i][t] === 0) {
+    for (let i = 0; i < boardstate.length; i++) {
+      for (let t = 0; t < boardstate.length; t++) {
+        if (boardstate[i][t] === 0) {
           zeroindex = i;
           zerosindex = t;
         }
       }
     }
-    if (zeroindex === index + 1 && zerosindex === sindex) {
-      console.log("swap");
-      var value = board[index][sindex];
-      var temp = [...board];
-      var temprowzero = board[index].splice(sindex, 1, 0);
-      var temprow = board[zeroindex].splice(zerosindex, 1, value);
-      console.log("Index: " + index);
-      console.log("Subindex: " + sindex);
-      console.log(board[index][sindex]);
-      console.log(temp);
-      var temp0 = board.splice(sindex, 1, [temprowzero]).flat();
-      var temp1 = [...board[1]];
-      var temp2 = [...board[2]];
-      var temp2 = board.splice(zeroindex, 1, [temprow]).flat();
-      console.log(temp);
-      console.log(temp0);
-      console.log(temp1);
-      console.log(temp2);
-      setboardstate([temp0, temp1, temp2]);
+
+    console.log("Board Index: " + boardstate[0]);
+    console.log("Board Index: " + board[1]);
+    console.log("Board Index: " + board[2]);
+
+    console.log("Zero Index was : " + zeroindex + " " + zerosindex);
+    console.log("Index length: " + index);
+    console.log("Total length: " + board[index]);
+    if (index < board.length - 1) {
+      if (board[index + 1][sindex] === board[zeroindex][zerosindex]) {
+        console.log("swap left");
+        var value = board[index][sindex];
+        var temp = [...board];
+        var temprowzero = board[index].splice(sindex, 1, 0);
+        var temprow = board[zeroindex].splice(zerosindex, 1, value);
+        var temp0 = board.splice(sindex, 1, [temprowzero]).flat();
+        var temp1 = [...board[1]];
+        var temp2 = [...board[2]];
+        var temp2 = board.splice(zeroindex, 1, [temprow]).flat();
+        setboardstate([temp0, temp1, temp2]);
+      }
+    }
+    if (index > 0) {
+      if (board[index - 1][sindex] === board[zeroindex][zerosindex]) {
+        console.log("swap right");
+      }
     }
   }
 
