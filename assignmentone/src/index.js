f079d14bc3ab7f4116a35e3c5840a69dff5e18e1
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-25 01:51:00 +0000

    the inital grid is setup

--- assignmentone/src/index.js
+++ assignmentone/src/index.js
@@ -1,10 +1,50 @@
 import react from "react";
+import { useState } from "react";
 import reactdom from "react-dom";
-import { ChakraProvider, Heading, Text, Link } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Center,
+  Heading,
+  Text,
+  Link,
+  Button,
+  HStack,
+  VStack,
+} from "@chakra-ui/react";
+
+function App() {
+  const board = [
+    [8, 7, 6],
+    [3, 4, 5],
+    [0, 2, 1],
+  ];
+
+  const cart = [
+    (5)[("Corn", "Potato", "Radish")],
+    (6)[("Tomato", "Graphes", "Mango")],
+    7,
+  ];
+
+  return (
+    <Center p="20">
+      <HStack>
+        {board.map((items, index) => {
+          return (
+            <VStack>
+              {items.map((sitem, sindex) => {
+                return <Button>{sitem}</Button>;
+              })}
+            </VStack>
+          );
+        })}
+      </HStack>
+    </Center>
+  );
+}
 
 reactdom.render(
   <ChakraProvider>
-    <Heading>Hello World</Heading>
+    <App />
   </ChakraProvider>,
   document.getElementById("root")
 );
