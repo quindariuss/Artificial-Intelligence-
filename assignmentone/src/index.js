2201729361ff9af2daa64c6a1817c4dafe19a812
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-25 14:29:03 +0000

    currently have an board and now need to make the buttons swap

--- assignmentone/src/index.js
+++ assignmentone/src/index.js
@@ -19,20 +19,49 @@ function App() {
     [0, 2, 1],
   ];
 
-  const cart = [
-    (5)[("Corn", "Potato", "Radish")],
-    (6)[("Tomato", "Graphes", "Mango")],
-    7,
-  ];
+  const [boardstate, setboardstate] = useState(board);
+
+  function handleClick(index, sindex) {
+    console.log("Button Pressed:" + index + ", " + sindex);
+
+    var zeroindex = 0;
+    var zerosindex = 0;
+
+    for (let i = 0; i < board.length; i++) {
+      for (let t = 0; t < board.length; t++) {
+        if (board[i][t] === 0) {
+          zeroindex = i;
+          zerosindex = t;
+        }
+      }
+    }
+    if (zeroindex === index + 1 && zerosindex === sindex) {
+      console.log("swap");
+      var temp = [...board];
+      var temprow = board[index].splice(sindex, 1, 10);
+      console.log(temp);
+      var temp0 = board.splice(sindex, 1, [temprow]);
+      var temp1 = [...board[1]];
+      var temp2 = [...board[2]];
+      console.log(temp);
+      console.log(temp0);
+      console.log(temp1);
+      console.log(temp2);
+    }
+  }
 
   return (
     <Center p="20">
       <HStack>
-        {board.map((items, index) => {
+        {boardstate.map((items, index) => {
           return (
             <VStack>
               {items.map((sitem, sindex) => {
-                return <Button>{sitem}</Button>;
+                return (
+                  <Button onClick={() => handleClick(index, sindex)}>
+                    {sitem}
+                  </Button>
+                );
               })}
             </VStack>
           );
