f0899126f6deba7111d0c26e15689c8d244d4454
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-07-22 03:14:46 +0000

    Setting up functions for transition images

--- assignment_three/index.js
+++ assignment_three/index.js
@@ -1,9 +1,13 @@
-/* DOM setup */
-
 const fileinput = document.getElementById("fileinput");
 const canvas = document.getElementById("canvas");
 const contex = canvas.getContext("2d");
 const source_image = new Image();
+const red_slider = document.getElementById("red");
+const green_slider = document.getElementById("green");
+const blue_slider = document.getElementById("blue");
+const brightness_slider = document.getElementById("brightness");
+const grayscale_slider = document.getElementById("grayscale");
+const contrast_slider = document.getElementById("contrast");
 let image_data = null;
 let orginal_pixels = null;
 let current_pixels = null;
@@ -25,6 +29,18 @@ source_image.onload = function () {
     source_image.width,
     source_image.height
   );
-
   orginal_pixels = image_data.data.slice();
 };
+
+function get_index(x, y) {
+  return (x + y * source_image.width) * 4;
+}
+
+function run_filter() {
+  for (let index = 0; index < source_image.height; index++) {
+    for (let subindex = 0; subindex < source_image.width; subindex++) {
+      // Apply filters
+    }
+  }
+  //Draw new Image
+}
